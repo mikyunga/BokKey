b13a544abe3e72249be5a62afb5a9c4d0cf9ce33
mikyung <seomk08@gmail.com>
2025-11-09 18:09:18 +0900

    CHORE: 기본설정1 테일윈드, eslint, vite react

---
+++ FE/src/main.jsx
@@ -0,0 +1,29 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { BookmarkProvider } from './contexts/BookMarkContext.jsx';
+import { CartProvider } from './contexts/CartContext'; // 추가
+
+async function prepare() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('../mocks/browser');
+    await worker.start();
+  }
+
+  const root = createRoot(document.getElementById('root'));
+  root.render(
+    <BrowserRouter>
+      <AuthProvider>
+        <BookmarkProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </BookmarkProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+prepare();
---
+++ FE/tailwind.config.js
@@ -0,0 +1,93 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard'],
+      },
+      colors: {
+        main: {
+          DEFAULT: 'rgb(var(--color-main) / <alpha-value>)',
+        },
+        filter: {
+          DEFAULT: 'rgb(var(--color-filter) / <alpha-value>)',
+        },
+        star: {
+          DEFAULT: 'rgb(var(--color-star) / <alpha-value>)',
+        },
+        black: {
+          DEFAULT: 'rgb(var(--color-black) / <alpha-value>)',
+          hover: 'rgb(var(--color-main-hover) / <alpha-value>)',
+          buttonStroke: 'rgba(59, 104, 239, 0.3)', // 버튼 테두리 고정값
+          buttonFill: 'rgba(59, 104, 239, 0.05)', // 버튼 fill 고정값
+          _01: 'rgba(0, 0, 0, 0.01)',
+          _02: 'rgba(0, 0, 0, 0.02)',
+          _03: 'rgba(0, 0, 0, 0.03)',
+          _04: 'rgba(0, 0, 0, 0.04)',
+          _05: 'rgba(0, 0, 0, 0.05)',
+          _06: 'rgba(0, 0, 0, 0.06)',
+          _07: 'rgba(0, 0, 0, 0.07)',
+          _10: 'rgba(0, 0, 0, 0.1)',
+          _30: 'rgba(0, 0, 0, 0.3)',
+          _50: 'rgba(0, 0, 0, 0.5)',
+          _60: 'rgba(0, 0, 0, 0.6)',
+          _70: 'rgba(0, 0, 0, 0.7)',
+        },
+        gray: {
+          DEFAULT: 'rgb(var(--color-gray) / <alpha-value>)',
+          stroke01: 'rgba(0, 0, 0, 0.01)',
+          stroke02: 'rgba(0, 0, 0, 0.02)',
+          stroke03: 'rgba(0, 0, 0, 0.03)',
+          stroke04: 'rgba(0, 0, 0, 0.04)',
+          stroke05: 'rgba(0, 0, 0, 0.05)',
+          stroke07: 'rgba(0, 0, 0, 0.07)',
+          stroke08: 'rgba(0, 0, 0, 0.08)',
+          stroke10: 'rgba(0, 0, 0, 0.1)',
+          stroke15: 'rgba(0, 0, 0, 0.15)',
+          stroke20: 'rgba(0, 0, 0, 0.2)',
+          stroke30: 'rgba(0, 0, 0, 0.3)',
+          stroke50: 'rgba(0, 0, 0, 0.5)',
+          stroke60: 'rgba(0, 0, 0, 0.6)',
+          stroke70: 'rgba(0, 0, 0, 0.7)',
+        },
+        white: {
+          DEFAULT: 'rgb(var(--color-white) / <alpha-value>)',
+          _05: 'rgba(255,255,255,0.5)',
+        },
+        border: {
+          DEFAULT: 'rgba(221, 221, 221, <alpha-value>)',
+        },
+        rederror: {
+          DEFAULT: 'rgba(255,88,88,1)',
+        },
+        login: {
+          DEFAULT: 'rgb(var(--color-login) / <alpha-value>)',
+        },
+      },
+      boxShadow: {
+        'custom-drop': '0px 0px 40px 0px rgba(0, 0, 0, 0.1)',
+        card: '0px 1px 10px rgba(0, 0, 0, 0.03)',
+        modal: '0px 30px 80px 0px rgba(0,0,0,0.15)',
+        custom: '0px 1px 4px 0px rgba(0,0,0,0.1)',
+        dropDown: '0px 0px 10px 0px rgba(0,0,0,0.03)',
+      },
+      animation: {
+        typing: 'typing 1s steps(16) alternate, blink .4s infinite',
+        fadeIn: 'fadeIn 0.3s ease-out forwards',
+        modalIn: 'modalIn 0.3s ease-out forwards',
+        float: 'float 1.5s ease-in-out infinite',
+
+        blinkFade: 'blinkFade 1s ease-in-out infinite',
+      },
+    },
+    plugins: [],
+    experimental: {
+      // Tailwind가 className 내부의 동적 표현식을 더 잘 감지하게 해주는 설정
+      classRegex: [
+        ['className="([^"]+)"', 1],
+        ['className={`([^`]+)`', 1],
+      ],
+    },
+  },
+};
