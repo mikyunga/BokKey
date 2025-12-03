d4401b77ff518e0015fe3130f4133dac7c9c1426
장서현 <jsh030213@gmail.com>
2025-12-03 18:10:34 +0900

    style: 색상 진하게

--- FE/src/components/common/map/SearchBar.jsx
+++ FE/src/components/common/map/SearchBar.jsx
@@ -11,9 +11,9 @@ export default function SearchBar({ searchQuery, setSearchQuery }) {
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="장소를 입력해주세요."
         className="w-full px-4 py-[10px] pr-9 
-                   bg-[rgba(0,0,0,0.02)] 
-                   border border-[rgba(0,0,0,0.015)] rounded-full 
-                   text-[16px] placeholder:text-[rgba(0,0,0,0.6)] 
+                   bg-[rgba(0,0,0,0.015)] 
+                   border border-[rgba(0,0,0,0.02)] rounded-full 
+                   text-[16px] placeholder:text-[rgba(0,0,0,0.5)] 
                    focus:outline-none focus:placeholder:text-[rgba(0,0,0,0)] 
                    focus:shadow-[0_1px_4px_rgba(0,0,0,0.03)] 
                    focus:bg-white focus:border-[rgba(0,0,0,0.05)] 
@@ -21,7 +21,7 @@ export default function SearchBar({ searchQuery, setSearchQuery }) {
       />
       <Search
         strokeWidth={1.75}
-        className="absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 text-[rgba(0,0,0,0.7)]"
+        className="absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 text-[rgba(0,0,0,0.5)]"
       />
     </div>
   );
