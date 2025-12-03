3d736ea7dd9f182e2ff33174ec760705f0582cf5
장서현 <jsh030213@gmail.com>
2025-12-03 16:46:22 +0900

    style: 서치바 디자인

--- FE/src/components/common/map/SearchBar.jsx
+++ FE/src/components/common/map/SearchBar.jsx
@@ -10,9 +10,12 @@ export default function SearchBar({ searchQuery, setSearchQuery }) {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="장소를 입력해주세요."
-        className="w-full px-3 py-2 pr-10 border border-gray-stroke10 rounded-lg text-sm placeholder:text-gray-stroke30"
+        className="w-full px-4 py-[10px] pr-9 bg-black-_02 border border-black-_01 rounded-full text-[16px] placeholder:text-black-_50 focus:outline-none focus:placeholder:text-transparent focus:shadow-[0_1px_4px_rgba(0,0,0,0.03)] focus:bg-white-_100 focus:border-black-_05 transition-colors duration-200 hover:shadow-sm"
       />
-      <Search className="absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-stroke30" />
+      <Search
+        strokeWidth={1.75}
+        className="absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 text-black-_50"
+      />
     </div>
   );
 }
